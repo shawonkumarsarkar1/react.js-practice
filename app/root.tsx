67e122f776689f0e4587ed75e81be231a3d5c030
shawonkumarsarkar1 <shawonkumarsarkar1@gmail.com>
2025-12-18 01:42:35 +0600

    Added the website theme using the browser default theme

--- app/root.tsx
+++ app/root.tsx
@@ -24,13 +24,17 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const themeInitializerScript = `function _0x5bde(t,e){return t-=157,_0x8c6a()[t]}function _0x8c6a(){var t=["222816jpdmPq","Oczdf","(prefers-c","3546irSzzV","196OeMapj","wnvNR","62pVWhhQ","679ItXhSw","ement","514175tBelWU","824ncKCfk","matches","documentEl","4ZPfnAA","WulXQ","add","dark","nBXAA","classList","matchMedia","e: dark)","olor-schem","746598zfMhDw","22kpbtoR","light","getItem","1313208FSsuAQ","4430qfFghb","theme"];return(_0x8c6a=function(){return t})()}(function(){for(var t=_0x5bde,e=_0x8c6a();;)try{if(134926==-parseInt(t(183))*(-parseInt(t(181))/2)+parseInt(t(170))/3+parseInt(t(161))/4*(-parseInt(t(157))/5)+-parseInt(t(174))/6+parseInt(t(184))/7*(-parseInt(t(158))/8)+-parseInt(t(180))/9*(-parseInt(t(175))/10)+parseInt(t(171))/11*(parseInt(t(177))/12))break;e.push(e.shift())}catch(t){e.push(e.shift())}})(),function(){var t=_0x5bde,e={WulXQ:t(176),nBXAA:t(179)+t(169)+t(168),Oczdf:t(164),wnvNR:t(172)};try{var n=localStorage[t(173)](e[t(162)]),a=window[t(167)](e[t(165)])[t(159)],a=n||(a?e[t(178)]:e[t(182)]);document[t(160)+t(185)][t(166)][t(163)](a)}catch(t){}}();`;
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script
+          dangerouslySetInnerHTML={{ __html: themeInitializerScript }}
+        ></script>
       </head>
       <body>
         {children}
